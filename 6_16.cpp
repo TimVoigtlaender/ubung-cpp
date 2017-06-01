2a275aa864825718bb4ba7aa3610fd563b6a25bb
TimVoigtlaender <tim-voigtlaender@gmx.de>
2017-06-01 03:50:40 +0200

    6 Kommentare

--- 6_16.cpp
+++ 6_16.cpp
@@ -11,22 +11,21 @@ int main()
 	int wertn = 12;
 	double x[wertn], fx[wertn];
 	
-	ifstream bucky;
+	ifstream bucky;																				// Daten aus Datei auslesen.
 	bucky.open("a16-interpol.dat");
-	
 	for(int k=0; k<wertn; k++){
 		bucky >> number;
 		x[k] = number;
 		bucky >> number;
 		fx[k] = number;
 	}
-	
 	bucky.close();
+	
 	ofstream myfile;
 	myfile.open("a16-interpol-res.dat");
 	cout << "Bitte x-Wert eingeben:" << endl;
 	
-	for(int m=0; wert!=0; m++){
+	for(int m=0; wert!=0; m++){																	// f(x) berechnen.
 		cin >> wert;
 			for(int i=0; i<wertn; i++){
 				for(int k=0; k<wertn; k++){
@@ -37,7 +36,7 @@ int main()
 				fvx += fx[i]*prod;
 				prod = 1;
 			}
-		cout << "f(" << wert << ")=" << fvx << endl;
+		cout << "f(" << wert << ")=" << fvx << endl;											// Tupel abspeichern.
 		myfile << wert << setw(10) << fvx << endl;	
 		fvx = 0;
 	}
