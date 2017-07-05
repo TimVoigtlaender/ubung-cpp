1f5732e428497077aa1a55b8538333b0b5e4e833
TimVoigtlaender <tim-voigtlaender@gmx.de>
2017-07-05 11:58:09 +0200

    fix

--- 10_26.cpp
+++ 10_26.cpp
@@ -3,11 +3,7 @@
 #include <stdlib.h> 
 using namespace std ;
 
-
-int anz = 8;
-int x[8];/* x[i] = x coordinate of queen in row i. */
-
-void ausgabe(){
+void ausgabe(int anz,int *x){
 	cout << "+";
 	for(int i=0; i<anz;i++){
 		cout << "---+";
@@ -33,29 +29,36 @@ void ausgabe(){
  	cout << endl;
 }
 
-int  free (int testcollum, int testrow){
+int  free (int testcollum, int testrow, int anz, int *x){
   	for (int test=0; test<testrow; test++)
     	if ((x[test]==testcollum) || (abs(x[test]-testcollum)==abs(test-testrow))) return 0;
   return 1;
 }
 
 
-void setqueen(int row){
+void setqueen(int row,int &count,int anz,int *x){
 	if (row== anz){
-  		ausgabe();
-		system ("PAUSE");
+		count++;
+// 		ausgabe(anz, x);
+//		system ("PAUSE");
 	}
 	else{
 	   	for (int i=0; i< anz; i++){
-    		if ( free(i,row)) {
+    		if ( free(i,row, anz, x)) {
         x[row]=i;
-        setqueen(row+1);
+        setqueen(row+1, count, anz, x);
       		}	
       	}
   	}
 }
 
 int main (){
-	setqueen(0);
+	int anz;
+	int count=0;
+	cout << "Wie gross ist das Feld?" << endl;
+	cin >> anz;
+	int x[anz];
+	setqueen(0, count, anz, x);
+	cout << "Es existieren " << count << " Moeglichkeiten die Damen aufs Feld zu stellen."<< endl; 
 	return 0;
 }
