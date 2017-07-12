304dd47c0c59db3920b5ef347e405e44b32a11d0
TimVoigtlaender <tim-voigtlaender@gmx.de>
2017-07-12 13:47:54 +0200

    fix

--- 11_28.cpp
+++ 11_28.cpp
@@ -2,8 +2,6 @@
 #include <cmath>
 using namespace std;
 
-double (*f)(double);
-
 double f1(double x){
     return cos(x)-x;
 }
@@ -14,20 +12,19 @@ double f3(double x){
     return pow(x,2)-5*x+sin(x)-1;
 }
 
-double nullstellen(double x0,double x1,double epsx, double epsf){
+double nullstellen( double (*f)(double), double x0,double x1,double epsx, double epsf){
 	double xt;
-
+	if(f(x0)*f(x1)>0){
+        cout << "Es gibt in diesem Intervall keine Nullstelle" << endl;
+        return 0;
+    }
     for(int i=0;abs(x0-x1)>=epsx and abs(f(xt))>=epsf; i++){
     	xt=(x0+x1)/2.;
         cout << "Schritt " << i << ":" << endl << "Das Intervall geht von " << x0 << " bis "<< x1 << endl << "Der Wert fuer f(" << xt << ") ist " << f(xt) << endl;
-		if(f(x0)*f(x1)>0){
-            cout << "Es gibt in diesem Intervall keine Nullstelle" << endl;
-            return 0;
-        }
-        else if(f(x0)*f(xt)>0){
+        if(f(x0)*f(xt)>0){
            x0 = xt;
         }
-        else if(f(xt)*f(x1)>0){
+        else{
            x1 = xt;
         }
     }
@@ -42,16 +39,13 @@ int main()
     cout << "Welche Funktion?" << endl;
     cin >> nf;
     if(nf==1){
-    	f=f1;
-    	null = nullstellen(-10,10,epsx,epsf);
+    	null = nullstellen(f1,-10,10,epsx,epsf);
     }
     else if(nf==2){
-    	f=f2;
-    	null = nullstellen(2,10,epsx,epsf);
+    	null = nullstellen(f2, 2,10,epsx,epsf);
     }
     else if(nf==3){
-    	f=f3;
-    	null = nullstellen(-5,5,epsx,epsf);
+    	null = nullstellen(f3,-5,5,epsx,epsf);
     }
     else{
         cout << "Gibt es nicht!" << endl;
